da98db346beda76085d1fa5a87c753ab77d83bd7
Ilayoub <jrindou.ayoub@gmail.com>
2024-06-25 14:39:38 -0700

    AirBnB clone V2

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+"""This is the amenity Class"""
+
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey, Table
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+import os
 from models.place import place_amenity
 
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
+    """This is the Class for Amenity
     Attributes:
         name: input name
     """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship("Place", secondary=place_amenity)
+    __tablename__ = 'amenities'
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False, default="")
+        place_amenities = relationship(
+            "Place", secondary=place_amenity)
+    else:
+        name = ""
